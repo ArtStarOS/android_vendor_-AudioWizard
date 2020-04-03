27b7e427cab455ec2e5fd137997fbd14f5af2369
ArtOS-Dev <ArtStarOS.Project@gmail.com>
2020-04-03 19:08:13 +0700

    Fix Mount File

--- config.mk
+++ config.mk
@@ -23,28 +23,58 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/AudioWizard/etc/permissions/privapp-permissions-com.asus.maxxaudio.audiowizard.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.asus.maxxaudio.audiowizard.xml \
     vendor/AudioWizard/etc/permissions/privapp-permissions-com.asus.maxxaudio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/privapp-permissions-com.asus.maxxaudio.xml \
-    vendor/AudioWizard/vendor/etc:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc \
-    vendor/AudioWizard/vendor/etc/dts:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts \
-    vendor/AudioWizard/vendor/etc/dts/acc:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc \
-    vendor/AudioWizard/vendor/lib:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib \
-    vendor/AudioWizard/vendor/lib/soundfx:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/soundfx \
-    vendor/AudioWizard/vendor/lib64:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib64 \
-    vendor/AudioWizard/vendor/lib64/soundfx:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib64/soundfx \
-    vendor/AudioWizard/vendor/lib64/soundfx:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib64/soundfx 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    vendor/AudioWizard/vendor/etc/dts/acc/d30de3a0-b177-4822-9c15-816b79545f82_usb_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/d30de3a0-b177-4822-9c15-816b79545f82_usb_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/d30de3a0-b177-4822-9c15-816b79545f82_usb_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/d30de3a0-b177-4822-9c15-816b79545f82_usb_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_bt_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_bt_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_bt_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_bt_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_usb_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_usb_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_usb_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_usb_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_wh_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a11_wh_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_bt_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_bt_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_bt_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_bt_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_usb_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_usb_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_usb_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_usb_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_wh_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a12_wh_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_bt_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_bt_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_bt_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_bt_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_usb_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_usb_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_usb_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_usb_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_wh_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a13_wh_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_bt_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_bt_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_bt_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_bt_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_usb_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_usb_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_usb_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_usb_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_wh_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a14_wh_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_bt_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_bt_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_bt_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_bt_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_usb_44.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_usb_44.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_usb_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_usb_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_wh_48.bin:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/acc/f08a4c48-0887-11e4-9191-0800200c9a15_wh_48.bin \
+    vendor/AudioWizard/vendor/etc/dts/current_bluetooth44k:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_bluetooth44k \
+    vendor/AudioWizard/vendor/etc/dts/current_bluetooth48k:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_bluetooth48k \
+    vendor/AudioWizard/vendor/etc/dts/current_lineout:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_lineout \
+    vendor/AudioWizard/vendor/etc/dts/current_lineout48k:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_lineout48k \
+    vendor/AudioWizard/vendor/etc/dts/current_speaker48k:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_speaker48k \
+    vendor/AudioWizard/vendor/etc/dts/current_speaker_off:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_speaker_off \
+    vendor/AudioWizard/vendor/etc/dts/current_speaker_on:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_speaker_on \
+    vendor/AudioWizard/vendor/etc/dts/current_usb44k:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_usb44k \
+    vendor/AudioWizard/vendor/etc/dts/current_usb48k:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/current_usb48k \
+    vendor/AudioWizard/vendor/etc/dts/customer.cfg:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/customer.cfg \
+    vendor/AudioWizard/vendor/etc/dts/default_config:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/default_config \
+    vendor/AudioWizard/vendor/etc/dts/default_speaker48:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/default_speaker48 \
+    vendor/AudioWizard/vendor/etc/dts/dts-eagle.lic:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/dts-eagle.lic \
+    vendor/AudioWizard/vendor/etc/dts/dts-m6m8-lic.key:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/dts-m6m8-lic.key \
+    vendor/AudioWizard/vendor/etc/dts/dts_hpx_settings:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/dts_hpx_settings \
+    vendor/AudioWizard/vendor/etc/dts/GEQ_configs:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/GEQ_configs \
+    vendor/AudioWizard/vendor/etc/dts/mode1_speaker48:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/mode1_speaker48 \
+    vendor/AudioWizard/vendor/etc/dts/mode2_speaker48:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/mode2_speaker48 \
+    vendor/AudioWizard/vendor/etc/dts/mode3_speaker48:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/mode3_speaker48 \
+    vendor/AudioWizard/vendor/etc/dts/mode4_speaker48:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/mode4_speaker48  \
+    vendor/AudioWizard/vendor/etc/dts/off_speaker48:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/off_speaker48  \
+    vendor/AudioWizard/vendor/etc/dts/offline_hptuning.db:$(TARGET_COPY_OUT_SYSTEM)/vendor/etc/dts/offline_hptuning.db \
+    vendor/AudioWizard/vendor/lib/libdts-eagle-shared.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/libdts-eagle-shared.so \
+    vendor/AudioWizard/vendor/lib/libdtsdsec.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/libdtsdsec.so \
+    vendor/AudioWizard/vendor/lib/soundfx/libdtsaudio.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib/soundfx/libdtsaudio.so \
+    vendor/AudioWizard/vendor/lib64/libdts-eagle-shared.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib64/libdts-eagle-shared.so \
+    vendor/AudioWizard/vendor/lib64/libdtsdsec.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib64/libdtsdsec.so \
+    vendor/AudioWizard/vendor/lib64/soundfx/libdtsaudio.so:$(TARGET_COPY_OUT_SYSTEM)/vendor/lib64/soundfx/libdtsaudio.so 
